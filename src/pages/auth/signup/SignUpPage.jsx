7227a27d027421ec780dbc4c6ee25caeb5a1b90f
ThuyPhuocThinh <thuyphuocthinhtpt1@gmail.com>
2024-11-01 16:22:54 +0700

    react query signup

--- src/pages/auth/signup/SignUpPage.jsx
+++ src/pages/auth/signup/SignUpPage.jsx
@@ -7,6 +7,8 @@ import { MdOutlineMail } from "react-icons/md";
 import { FaUser } from "react-icons/fa";
 import { MdPassword } from "react-icons/md";
 import { MdDriveFileRenameOutline } from "react-icons/md";
+import { useMutation } from "@tanstack/react-query";
+import toast from "react-hot-toast";
 
 export default function SignUpPage() {
   const [formData, setFormData] = useState({
@@ -16,16 +18,52 @@ export default function SignUpPage() {
     password: "",
   });
 
+  const resetFormData = () => {
+    setFormData({
+      email: "",
+      username: "",
+      fullName: "",
+      password: "",
+    });
+  };
+
+  const { mutate, isError, isPending, error } = useMutation({
+    mutationFn: async ({ email, username, fullName, password }) => {
+      try {
+        const res = await fetch("/api/auth/signup", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ email, username, fullName, password }),
+        });
+
+        // if (!res.ok) throw new Error("Something went wrong");
+        const data = await res.json();
+        if (data.error) {
+          throw new Error(data.error);
+        }
+        toast.success("Signup successfully");
+        resetFormData();
+        return data;
+      } catch (error) {
+        console.error(error);
+        toast.error(error.message);
+      }
+    },
+    onError: () => {
+      toast.error(error.message);
+    },
+  });
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(formData);
+    mutate(formData);
   };
 
   const handleInputChange = (e) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
   };
-
-  const isError = false;
 
   return (
     <div className="max-w-screen-xl mx-auto flex h-screen px-10">
@@ -86,9 +124,9 @@ export default function SignUpPage() {
             />
           </label>
           <button className="btn rounded-full btn-primary text-white">
-            Sign up
+            {isPending ? "Loading..." : "Sign up"}
           </button>
-          {isError && <p className="text-red-500">Something went wrong</p>}
+          {isError && <p className="text-red-500">{error.message}</p>}
         </form>
         <div className="flex flex-col lg:w-2/3 gap-2 mt-4">
           <p className="text-white text-lg">Already have an account?</p>
