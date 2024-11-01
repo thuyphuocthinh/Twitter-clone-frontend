87909b82fd68aebb92aba995fd29f6cda3c9fbc2
ThuyPhuocThinh <thuyphuocthinhtpt1@gmail.com>
2024-11-01 15:35:35 +0700

    set up UI

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/home/HomePage";
+import LoginPage from "./pages/auth/login/LoginPage";
+import SignUpPage from "./pages/auth/signup/SignUpPage";
+import Sidebar from "./components/common/Sidebar";
+import RightPanel from "./components/common/RightPanel";
+import NotificationPage from "./pages/notification/NotificationPage";
+import ProfilePage from "./pages/profile/ProfilePage";
+
+function App() {
+  return (
+    <div className="flex max-w-6xl mx-auto">
+      <Sidebar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/notifications" element={<NotificationPage />} />
+        <Route path="/profile/:username" element={<ProfilePage />} />
+      </Routes>
+      <RightPanel />
+    </div>
+  );
+}
+
+export default App;
