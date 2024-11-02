4063b0bb3ad510c865efa9213a12325ad5b5256e
ThuyPhuocThinh <thuyphuocthinhtpt1@gmail.com>
2024-11-02 20:32:13 +0700

    notification

--- src/components/common/Post.jsx
+++ src/components/common/Post.jsx
@@ -8,6 +8,7 @@ import { Link } from "react-router-dom";
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import toast from "react-hot-toast";
 import LoadingSpinner from "./LoadingSpinner";
+import { formatPostDate } from "../../utils/date/function";
 
 const Post = ({ post }) => {
   const [comment, setComment] = useState("");
@@ -67,11 +68,35 @@ const Post = ({ post }) => {
     },
   });
 
+  const { mutate: commentPost, isPending: isCommenting } = useMutation({
+    mutationFn: async () => {
+      try {
+        const res = await fetch(`/api/posts/comment/${post._id}`, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ text: comment }),
+        });
+        const data = await res.json();
+        if (!res.ok) {
+          throw new Error(data.error || "Something went wrong");
+        }
+        toast.success("Comment successfully");
+        setComment("");
+        queryClient.invalidateQueries({ queryKey: ["posts"] });
+        return data;
+      } catch (error) {
+        toast.error(error.message);
+        throw new Error(error);
+      }
+    },
+  });
+
   const postOwner = post.user;
   const isLiked = post.likes.includes(authUser._id);
   const isMyPost = authUser._id === post.user._id;
-  const formattedDate = "1h";
-  const isCommenting = false;
+  const formattedDate = formatPostDate(post.createdAt);
 
   const handleDeletePost = () => {
     deletePost();
@@ -79,6 +104,8 @@ const Post = ({ post }) => {
 
   const handlePostComment = (e) => {
     e.preventDefault();
+    if (isCommenting) return;
+    commentPost();
   };
 
   const handleLikePost = () => {
--- src/pages/notification/NotificationPage.jsx
+++ src/pages/notification/NotificationPage.jsx
@@ -4,32 +4,50 @@ import LoadingSpinner from "../../components/common/LoadingSpinner";
 import { IoSettingsOutline } from "react-icons/io5";
 import { FaUser } from "react-icons/fa";
 import { FaHeart } from "react-icons/fa6";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import toast from "react-hot-toast";
 
 const NotificationPage = () => {
-  const isLoading = false;
-  const notifications = [
-    {
-      _id: "1",
-      from: {
-        _id: "1",
-        username: "johndoe",
-        profileImg: "/avatars/boy2.png",
-      },
-      type: "follow",
+  const { data: notifications, isLoading } = useQuery({
+    queryKey: ["notifications"],
+    queryFn: async () => {
+      try {
+        const res = await fetch("/api/notifications");
+        const data = await res.json();
+        if (!res.ok) {
+          throw new Error(data.error || "Something went wrong");
+        }
+        return data;
+      } catch (error) {
+        throw new Error(error.message);
+      }
     },
-    {
-      _id: "2",
-      from: {
-        _id: "2",
-        username: "janedoe",
-        profileImg: "/avatars/girl1.png",
-      },
-      type: "like",
+  });
+
+  const queryClient = useQueryClient();
+
+  const { mutate: deleteNotificationsMutation } = useMutation({
+    mutationFn: async () => {
+      try {
+        const res = await fetch("/api/notifications/delete", {
+          method: "DELETE",
+        });
+        const data = await res.json();
+        if (!res.ok) {
+          throw new Error(error.message || "Something went wrong");
+        }
+        toast.success("Deleted notifications successfully");
+        queryClient.invalidateQueries({ queryKey: ["notifications"] });
+        return data;
+      } catch (error) {
+        toast.error(error.message);
+        throw new Error(error.message);
+      }
     },
-  ];
+  });
 
   const deleteNotifications = () => {
-    alert("All notifications deleted");
+    deleteNotificationsMutation();
   };
 
   return (
